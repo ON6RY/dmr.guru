4e4f03d8dff00daab97d11987734bcd8961ea7e4
Joeri Van Dooren <ure@on3ure.be>
2021-05-19 00:25:11 +0200

    fix

--- rpi_gpio_init.sh
+++ rpi_gpio_init.sh
@@ -31,11 +31,10 @@ echo 20 > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio20/direction
 
 touch /tmp/booting
-FILE=/tmp/booting
 
 while :
 do
-        if test -f "${FILE}"; then
+        if test -f "/tmp/booting"; then
                 echo 1 > /sys/class/gpio/gpio20/value && sleep 1 && echo 0 > /sys/class/gpio/gpio20/value && sleep 1
         else
                 echo 1 > /sys/class/gpio/gpio20/value
